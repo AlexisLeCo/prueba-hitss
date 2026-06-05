911fc74a95a8428bea631f2547625893347730bc
Alexis Ledesma <alexisledesma31@hotmail.com>
2026-06-04 21:24:30 -0600

    Se crea el formulario de informacion personal

---
+++ src/components/Forms/InputField.jsx
@@ -0,0 +1,20 @@
+import "./InputField.css";
+
+function InputField({
+  label,
+  placeholder,
+  type = "text",
+}) {
+  return (
+    <div className="input-group">
+      <label>{label}</label>
+
+      <input
+        type={type}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
+
+export default InputField;
--- src/pages/Step2.jsx
+++ src/pages/Step2.jsx
@@ -1,8 +1,11 @@
+import "./Step2.css";
+
 import WizardLayout from "../components/Layout/WizardLayout";
 import StepSidebar from "../components/Sidebar/StepSidebar";
 import PrimaryButton from "../components/Buttons/PrimaryButton";
+import InputField from "../components/Forms/InputField";
 
-function Step2({ prevStep }) {
+function Step2({ nextStep, prevStep }) {
   return (
     <WizardLayout
       sidebar={
@@ -13,11 +16,62 @@ function Step2({ prevStep }) {
         />
       }
     >
-      <h2>Paso 2</h2>
+      <div className="form-grid">
+        <InputField
+          label="Nombre"
+          placeholder="Ingrese su nombre"
+        />
 
-      <PrimaryButton onClick={prevStep}>
-        Regresar
-      </PrimaryButton>
+        <InputField
+          label="Número de Documento"
+          placeholder="Documento"
+        />
+
+        <InputField
+          label="Tipo de Documento"
+          placeholder="CC"
+        />
+
+        <InputField
+          label="Fecha de Nacimiento"
+          placeholder="00/00/0000"
+        />
+
+        <InputField
+          label="Correo Electrónico"
+          placeholder="correo@empresa.com"
+        />
+
+        <InputField
+          label="Ciudad / Municipio"
+          placeholder="Ciudad"
+        />
+
+        <InputField
+          label="Domicilio"
+          placeholder="Dirección"
+        />
+
+        <InputField
+          label="Barrio"
+          placeholder="Barrio"
+        />
+
+        <InputField
+          label="Número de Celular"
+          placeholder="3001234567"
+        />
+      </div>
+
+      <div className="buttons">
+        <PrimaryButton onClick={prevStep}>
+          Regresar
+        </PrimaryButton>
+
+        <PrimaryButton onClick={nextStep}>
+          Siguiente
+        </PrimaryButton>
+      </div>
     </WizardLayout>
   );
 }
